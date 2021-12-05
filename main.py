aae19e48daec4ffc84bfda0f73c2ff03cb3af49b
Joel McCandless <mail@joelmccandless.com>
2021-12-04 19:29:33 -0500

    Added a bunch of content to the senate page and put anchor links in it. Now, I will need to make some sort of a javascript function that will make navigating the links easier (sticky position, maybe?)

--- main.py
+++ main.py
@@ -407,11 +407,261 @@ function vote(element_ID) {
         self.send_response(200)
         self.end_headers()
         self.wfile.write('''<html><body>'''.encode('utf8'))
-        self.wfile.write('''<h2 id='about'>About the Senate</h2>
-Welcome to the Lexington High School Student-Faculty Senate! The Senate convenes at 3:15pm on Wednesdays in the Library Media Center.
+        self.wfile.write('''Click <a href='https://senate.lex.ma/constitution/'>here</a> to see the Senate's website!'''.encode('utf8'))
+        self.wfile.write('''<h2><a name='welcome'>Welcome!</a></h2>
+Welcome to the Lexington High School Student-Faculty Senate! The Senate convenes at 3:15pm on Wednesdays in the Library Media Center.<br />
 We implement school-wide policies on a number of issues, from things as mundane as placing extra benches around the school to changes as significant as eliminating the community service requirement for open campus, allowing students to eat in the Quad, or determining what information will be printed on transcripts.<br />
-All meetings are open to the public! If you want to change something about the school, we would love to hear and discuss your ideas.'''.encode('utf8'))
-        self.wfile.write(f'''<h2 id='meet'>Meet the Senators</h2>
+All meetings are open to the public! If you want to change something about the school, we would love to hear and discuss your ideas.<br />'''.encode('utf8'))
+        self.wfile.write('''<h2><a name='about'>About the Senate</a></h2>'''.encode('utf8'))
+        self.wfile.write('''<h2><a name='constitution'>Constitution</a></h2>
+Article I: Philosophy<br />
+<br />
+All members of the school community should have a meaningful voice in determining the policies of the school, in promoting a positive school climate, and in shaping the future of the school. It is essential that each member be kept informed through effective communications and have the power to influence decisions made at Lexington High School. For this purpose the Lexington High School Senate is established.<br />
+<br />
+<br />
+Article II: Membership<br />
+<br />
+Section 1 The Senate shall consist of two elected groups. The first shall include one certified staff representative for every ten members of the certified high school staff. The second shall include one student representative for every fifty members of the student body. The number of student representatives shall be determined each April 1 and be based on the current enrollment in grades eight through eleven.<br />
+<br />
+Section 2 Nine students shall be elected at-large from and by each class. If the total number of student representatives falls below thirty-six, then the number of representatives from each class shall be determined by dividing the total number of student representatives by four, with the first remainder allotted to the senior class, the second to the junior class, and the third to the sophomore class, in that order.<br />
+<br />
+Section 3 One faculty representative shall be elected from each building and one from the science building and one from the combined members of the music, art, industrial technology, and physical education departments. The remaining faculty representatives shall be elected from the faculty at large.<br />
+<br />
+Section 4 There will be up to five places open for groups who feel that they are not represented in the above election plan. These groups must petition the Senate for such representation.<br />
+<br />
+<br />
+Article III: Organization<br />
+<br />
+Section 1 The Senate shall have a Moderator, an Assistant Moderator, and a Secretary.<br />
+<br />
+Section 2 There shall be standing committees in the following areas: Executive, Social Action, Communications, Elections, Policy, School Climate.<br />
+<br />
+Section 3 Standing committees shall contain not fewer than four nor more than twelve members and shall approximate the student-faculty ratio of the Senate.<br />
+<br />
+Section 4 All members of the Senate shall serve on at least one standing committee.<br />
+<br />
+Section 5 The executive committee shall consist of the moderator, assistant moderator, secretary, and the Student Activities Coordinator. Members of the executive committee shall not serve on any other standing committees with the exception of the secretary, who shall chair the standing committee on communications.<br />
+<br />
+Section 6 In addition to the standing committees listed in Section 2, others may be established according to need by a simple majority vote of the Senate.<br />
+<br />
+<br />
+Article IV: Elections and Tenure of Office<br />
+<br />
+Section 1 The student members shall be elected by or on the third Friday in May. Freshman class representatives shall be elected by or on the second Friday in October. Faculty representatives shall be elected by the third Friday in May.<br />
+<br />
+Section 2 There shall be a moderator elected by and from the Senate for a one year term. If the moderator elected in any given year is a student, then the assistant moderator elected by the Senate shall be a faculty member; conversely, if the moderator is a faculty member, the assistant moderator shall be a student.<br />
+<br />
+Section 3 The moderator, the assistant moderator, and the secretary shall be elected by the full Senate at its first meeting following the May elections, but no later than the first day of June.<br />
+<br />
+Section 4 Each representative shall assume office one week after the conclusion of elections and shall serve until the next year's election.<br />
+<br />
+<br />
+Article V: Roles of the Officers and Standing Committees<br />
+<br />
+Section 1 Moderator<br />
+<br />
+A. The moderator shall preside over all meetings of the Senate as specified in Article VII.<br />
+B. The moderator shall be responsible for maintaining an orderly meeting and shall have the right to dismiss anyone disrupting a meeting.<br />
+<br />
+C. The moderator shall chair executive committee.<br />
+<br />
+D. The moderator and the assistant moderator shall meet weekly with the principal.<br />
+<br />
+E. The moderator shall facilitate and coordinate the work and efforts of the standing committees.<br />
+<br />
+<br />
+Section 2 Assistant Moderator<br />
+<br />
+A. The assistant moderator shall preside over the meetings of the Senate in the absence of the moderator.<br />
+B. The assistant moderator shall be a member of the executive committee.<br />
+<br />
+C. The assistant moderator and the moderator shall meet weekly with the principal.<br />
+<br />
+D. The assistant moderator shall facilitate and coordinate the work and efforts of the standing committees.<br />
+<br />
+<br />
+Section 3 Secretary<br />
+<br />
+A. The secretary shall record Senate attendance, make public minutes of Senate sessions, and carry on correspondence as may be directed by the moderator or the assistant moderator.<br />
+B. The secretary shall preside over Senate meetings in the absence of both the moderator and the assistant moderator.<br />
+<br />
+C. The secretary shall be a member of the executive committee.<br />
+<br />
+D. The secretary shall chair the standing committee on communications.<br />
+<br />
+Section 4 Student Activities Coordinator<br />
+<br />
+A. The Student Activities Coordinator shall act as advisor to the officers and to the Senate.<br />
+B. The Student Activities Coordinator shall serve as an exofficio and non-voting member of the Senate.<br />
+<br />
+C. The Student Activities Coordinator shall serve as a non-voting member of the executive committee.<br />
+<br />
+D. The Student Activities Coordinator shall assist the Senate by facilitating the various activities of the Senate.<br />
+<br />
+E. The Student Activities Coordinator, in the absence of all officers, shall preside over a meeting of the Senate and shall appoint a secretary for that particular meeting.<br />
+<br />
+<br />
+Section 5 Standing Committees<br />
+<br />
+A. The standing committees shall perform tasks as directed by the moderator and/or assistant moderator.<br />
+B. The standing committees shall meet to investigate issues within the scope of their charge and report their findings to the Senate for discussion and vote.<br />
+<br />
+C. The executive committee shall set agendas for the Senate meetings and shall appoint Senate members to standing committees in accordance with these by-laws.<br />
+<br />
+<br />
+Article VI: Scope and Jurisdiction<br />
+<br />
+Section 1 All matters of concern to the school community are appropriate for consideration by the Lexington High School Senate.<br />
+<br />
+Section 2 Any matter formulated as a bill and passed by the Senate in accordance with the provisions of Article VII, Section 4, shall be submitted to the administration in accordance with Article VII, Section 4, unless any portion of the bill:<br />
+<br />
+A. Contradicts state or federal law;<br />
+B. Interferes with the allotment of school department funds;<br />
+<br />
+C. Interferes with collective bargaining agreements;<br />
+<br />
+D. Impinges upon individual administrative and teacher evaluation;<br />
+<br />
+E. Impinges upon individual teachers' course organization and evaluation of students.<br />
+<br />
+<br />
+Article VII: Procedures<br />
+<br />
+Section 1 The Lexington High School Senate shall operate as a representative town meeting.<br />
+<br />
+Section 2 The Lexington High School Senate shall employ Robert's Rules of Order Newly Revised as the parliamentary reference for its deliberations.<br />
+<br />
+Section 3 For the transaction of business to occur within the Senate, a quorum of two-thirds of the Senate shall be required.<br />
+<br />
+Section 4 Voting Procedures with respect to bills<br />
+<br />
+A. Bills must have been submitted to the secretary at least five school days before a Senate meeting at which the bill may be considered.<br />
+B. Bills must have been published at least four school days before a Senate meeting day at which the bill may be considered.<br />
+<br />
+C. By a two-thirds vote of those members present and voting, the Senate may consider a late-filed bill.<br />
+<br />
+D. The Senate shall establish standing rules for the receipt and publication of bills.<br />
+<br />
+E. The executive committee shall place a bill on the agenda or assign the bill to a standing committee of the Senate for research, discussion, and recommendation of the Senate.<br />
+<br />
+F. Majority of the Senate present and voting shall be required to pass a bill.<br />
+<br />
+G. Following an affirmative Senate vote on a bill:<br />
+<br />
+i. The principal may give his written approval to the bill, in which case the bill shall take effect.<br />
+ii. The principal may decline to approve the bill, offer his objections to the same in writing, and then return the bill to the Senate.<br />
+<br />
+iii. The Senate may accept the principal's disapproval and the bill is then lost, or Senate may reinstitute the bill by a three-fourths majority of the members present and voting.<br />
+<br />
+iv. If the Senate votes to reinstitute, the Secretary shall transmit the bill to the Lexington School Committee. After ten school days and in the absence of a recorded vote of the School Committee, the bill shall take effect.<br />
+<br />
+v. If the principal allows a bill to remain on his desk for ten school days without a response, the bill should take effect.<br />
+<br />
+<br />
+Article VIII: Meetings<br />
+<br />
+Section 1 The Senate shall meet once each week when school is in session.<br />
+<br />
+Section 2 All meetings of the Senate are open.<br />
+<br />
+Section 3 Exclusivity of Senate meeting times.<br />
+<br />
+A.The Senate shall meet once each week at a designated and specific time during normal school hours.<br />
+B. This time shall be reserved by the entire Lexington High School community exclusively for Senate meetings.<br />
+<br />
+C. The scheduling of practices, rehearsals, and other required meetings is strictly prohibited during Senate meeting time.<br />
+<br />
+D. Senate meetings shall in no way impinge upon or interfere with regular class meeting times.<br />
+<br />
+E. No teacher shall be assigned any involuntary professional duty while the Senate is in session.<br />
+<br />
+Section 4 Each spring the Senate shall evaluate its meeting time and establish specific meeting times for the next school year.<br />
+<br />
+<br />
+Article IX: Senate Attendance<br />
+<br />
+Dismissal and reinstatement of Senate members<br />
+<br />
+A.The secretary shall certify the attendance of members at Senate meetings and shall excuse the absence of members for good cause. After December 1, any member whose unexcused absences from Senate meetings shall amount to twenty percent of the total number of Senate meetings since the beginning of the school year shall be deemed to have resigned, and a vacancy shall be declared by the secretary.<br />
+B.Within five days following such declaration, the former member may request a hearing before the standing committee on elections.<br />
+<br />
+C.Following such a hearing the standing committee on elections may reinstate the former Senate member.<br />
+<br />
+D. If five days elapse without a request for a hearing, or if the standing committee on elections does not reinstate the former Senate member, then the procedures established under Article X for filling vacancies shall take effect.<br />
+<br />
+<br />
+Article X: Senate Vacancies<br />
+<br />
+Section 1 When any seat in the Senate is declared vacant, the seat shall be filled by the candidate who represents the constituency of the vacated seat and who attained the next highest number of votes in the most recent Senate elections for that seat.<br />
+<br />
+Section 2 When any office in the Senate is permanently vacated, a successor shall be elected by and from the Senate to serve the unexpired term in accordance with Article IV, Section 2.<br />
+<br />
+<br />
+Article XI: School Elections<br />
+<br />
+The standing committee on elections, authorized under Article III, Section 2, shall conduct all school elections.<br />
+<br />
+<br />
+Article XII: Bill of Rights<br />
+<br />
+The Senate shall support and defend the following rights of all members of the school community:<br />
+<br />
+A. To express freely and peaceably, in speech and in writing, opinions and ideas;<br />
+B. To distribute printed materials on school grounds before school, during school, and after school hours;<br />
+<br />
+C. To assemble freely and peaceably in any manner, before school, during school, and after school, so long as such gatherings do disrupt the educational process;<br />
+<br />
+D. To defend against an accusation before any discipline, suspension, expulsion, termination, or other major action may occur;<br />
+<br />
+E. To petition for redress of grievances;<br />
+<br />
+F. To be free from physical and verbal harassment.<br />
+<br />
+<br />
+Article XIII: Board of Appeals<br />
+<br />
+Section 1 A board of appeals shall be organized to address student and teacher grievances that arise from Lexington High School policies, rules, regulations, and procedures.<br />
+<br />
+Section 2 The board of appeals shall consist of student and faculty members in the same proportion as the representatives in the whole Senate. Student and faculty members shall be elected by and from their respective constituencies. No person may serve simultaneously as a member of the Senate and of the board of appeals.<br />
+<br />
+Section 3 The Senate shall establish procedures and rules for the operation of the board of appeals.<br />
+<br />
+<br />
+Article XIV: Amendments<br />
+<br />
+Section 1 Amendments to this Constitution may be proposed by a two-thirds majority of the Senate members present and voting.<br />
+<br />
+Section 2 Amendments to this constitution shall take effect after ratification in separate elections by a majority vote of students and by a majority vote of faculty.<br />
+<br />
+<br />
+Appendix to the Constitution Clarifying Article VIII, Section 3<br />
+<br />
+In order to implement Article VIII, Section 3 of the Constitution, the following regulations and procedures shall be adopted for the Lexington High School Community:<br />
+<br />
+A. The weekly school schedule shall be arranged so that on one designated day every week, for the purpose of Senate meetings, school shall end officially one hour earlier than on other days.<br />
+B. All students and faculty not participating in the Senate deliberations shall be dismissed at that time.<br />
+<br />
+C. During this Senate meeting period, school shall have been terminated for the day; hence, there shall be no classes in session and no supervision of non-Senate member students by the faculty.<br />
+<br />
+D. This arrangement shall not decrease the amount of classroom time currently scheduled.<br />
+<br />
+E. No student or faculty member participating in Senate deliberations shall be penalized in any way for not participating in athletic, dramatic, debate, or club activities during the time designated for Senate meetings.<br />'''.encode('utf8'))
+        self.wfile.write('''<h2><a name='FAQ'>Frequently Asked Questions</a></h2>
+When and where does the Senate meet?<br />
+The Senate meets every Wednesday afternoon after school in the Library Media Center.<br />
+<br />
+What happened to X-Block?<br />
+X-Block was originally created as a time for Senate to meet. The Department of Education has found that X-Block did not engage the time of a sufficient number of students to be counted as a Time and Learning hour. The school will be discussing how to restore X-Block to the schedule this year (2014-2015).<br />
+<br />
+Are Senate meetings open to those who aren't members?<br />
+Yes. Anyone who wishes to attend a senate meeting may do so.<br />
+<br />
+How many Senators are there?<br />
+There are up to 10 faculty senators, along with approximately 10 student senators elected per grade. See the Constitution for details.<br />
+<br />
+What are Senate Committees?<br />
+Each senator is assigned to a Committee at the beginning of the year. There are 6 committees: Executive, Community Service, Oversight, Climate, Policy, and Communications. Each committee meets once a week outside of the Senate meeting.'''.encode('utf8'))
+        self.wfile.write(f'''<h2><a name='meet'>Meet the Senators</a></h2>
 Executive<br />
 {local.EXECUTIVE}
 
