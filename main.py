b0aef90c2359bc1348d49d3fffe39d87c7942fb9
Joel McCandless <mail@joelmccandless.com>
2022-01-16 14:51:07 -0500

    I made the prints when the server starts up under DEBUG = 0 because they were making server turnaround really really slow. Without the prints, the turnaround is fast again.

--- main.py
+++ main.py
@@ -1702,24 +1702,25 @@ class invalidCookie(ValueError):
 def main():
     print('Student Change Web App... running...')
 
-    print(f'\n{db.user_cookies=}')
-    for cookie, user in db.user_cookies.items():
-        print(f'  {cookie} : User({user.email}, {user.cookie_code}, {user.activity}, {user.votes}, {user.verified_email})')
+    if MyHandler.DEBUG == 0:
+        print(f'\n{db.user_cookies=}')
+        for cookie, user in db.user_cookies.items():
+            print(f'  {cookie} : User({user.email}, {user.cookie_code}, {user.activity}, {user.votes}, {user.verified_email})')
 
-    print(f'\n{db.verification_links=}')
-    for link, ID in db.verification_links.items():
-        print(f'  {link} : {ID}')
+        print(f'\n{db.verification_links=}')
+        for link, ID in db.verification_links.items():
+            print(f'  {link} : {ID}')
 
-    print(f'\n{db.opinions_database=}')
-    for ID, opinion in db.opinions_database.items():
-        print(f'  {ID} : Opinion({opinion.ID}, {opinion.text}, {opinion.activity}, {opinion.approved})')
+        print(f'\n{db.opinions_database=}')
+        for ID, opinion in db.opinions_database.items():
+            print(f'  {ID} : Opinion({opinion.ID}, {opinion.text}, {opinion.activity}, {opinion.approved})')
 
-    print(f'\n{db.opinions_calendar=}')
-    sorted_calendar = list(db.opinions_calendar.keys())
-    sorted_calendar.sort()
-    for this_date in sorted_calendar:
-        ID_set = db.opinions_calendar[this_date]
-        print(f'  {this_date} : {ID_set}')
+        print(f'\n{db.opinions_calendar=}')
+        sorted_calendar = list(db.opinions_calendar.keys())
+        sorted_calendar.sort()
+        for this_date in sorted_calendar:
+            ID_set = db.opinions_calendar[this_date]
+            print(f'  {this_date} : {ID_set}')
 
         
     httpd = ReuseHTTPServer(('0.0.0.0', 8888), MyHandler)
