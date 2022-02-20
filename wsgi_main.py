400e8d3ecfcbbde81452fcbef710890d84da17b4
Joel McCandless <mail@joelmccandless.com>
2022-02-20 15:00:54 -0500

    Make the schedule calendar show the periods It is much more intuitive that the calendar shows Sunday-Tuesday, Wednesday alone, and then Thursday-Saturday.

--- wsgi_main.py
+++ wsgi_main.py
@@ -1280,19 +1280,29 @@ td {
             for day_of_week in range(7):
                 self.wfile.write(f'<td>{calendar.day_name[(day_of_week + 6) % 7]}</td>'.encode('utf8'))
             self.wfile.write('</tr><tr>'.encode('utf8'))
-            for day in range((calendar.monthrange(today_date.year, today_date.month)[0] + 1) % 7):
+            first_day = calendar.monthrange(today_date.year, today_date.month)[0]
+            month_days = calendar.monthrange(today_date.year, today_date.month)[1]
+            for day in range((first_day + 1) % 7):
                 self.wfile.write('<td></td>'.encode('utf8'))
-            for day_number in range(1, calendar.monthrange(today_date.year, today_date.month)[1] + 1):
+            for day_number in range(1, month_days + 1):
                 this_date = datetime.date(today_date.year, today_date.month, day_number)
                 already_selected = 0
                 if str(this_date) in db.opinions_calendar:
                     already_selected = len(db.opinions_calendar[str(this_date)])
                 if (this_date.weekday() + 1) % 7 == 0 and not day_number == 1:
                     self.wfile.write('</tr>'.encode('utf8'))
-                    if not day_number == calendar.monthrange(today_date.year, today_date.month)[1]:
+                    if not day_number == month_days:
                         self.wfile.write('<tr>'.encode('utf8'))
-                self.wfile.write(f'''<td onclick='document.location.href="/schedule_date?date={this_date}"'>{day_number}<br />{already_selected}/10</td>'''.encode('utf8'))
-            for day in range(35 - calendar.monthrange(today_date.year, today_date.month)[1] - (calendar.monthrange(today_date.year, today_date.month)[0] + 1) % 7):
+                if day_number == 1:
+                    self.wfile.write(f'''<td onclick='document.location.href="/schedule_date?date={this_date}"' colspan='{3 - ((this_date.weekday() + 1) % 7) % 4}'>{day_number}-{first_day}<br />{already_selected}/10</td>'''.encode('utf8'))
+                if ((this_date.weekday() + 1) % 7) % 4 == 0:
+                    if day_number + 3 > month_days:
+                        self.wfile.write(f'''<td onclick='document.location.href="/schedule_date?date={this_date}"' colspan='{day_number + 3 - month_days}'>{day_number}-{month_days}<br />{already_selected}/10</td>'''.encode('utf8'))
+                    else:
+                        self.wfile.write(f'''<td onclick='document.location.href="/schedule_date?date={this_date}"' colspan='3'>{day_number}, {day_number + 1}, {day_number + 2}<br />{already_selected}/10</td>'''.encode('utf8'))
+                elif this_date.weekday() == 2:
+                    self.wfile.write(f'''<td onclick='document.location.href="/schedule_date?date={this_date}"'>{day_number}<br />{already_selected}/10</td>'''.encode('utf8'))                        
+            for day in range(35 - month_days - (first_day + 1) % 7):
                 self.wfile.write('<td></td>'.encode('utf8'))
             self.wfile.write('</tr>'.encode('utf8'))
             self.wfile.write('</table></article>'.encode('utf8'))
