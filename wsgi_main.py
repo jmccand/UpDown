4f09e735db0594e5a39e111afa9f8b1358b732ec
Joel McCandless <mail@joelmccandless.com>
2022-02-21 16:48:03 -0500

    Get rid of the link to the Senate's website

--- wsgi_main.py
+++ wsgi_main.py
@@ -938,7 +938,7 @@ article {
         self.wfile.write('</head><body>'.encode('utf8'))
         self.send_links_body()
         self.wfile.write('''<div id='back_to_top'><a href='/senate#top'>Back to the top</a></div>'''.encode('utf8'))
-        self.wfile.write('''<article>Click <a name='top' href='https://senate.lex.ma/constitution/'>here</a> to see the Senate's website!'''.encode('utf8'))
+        self.wfile.write('''<article>'''.encode('utf8'))
         self.wfile.write('''<h2><a name='TOC'>Table of Contents</a></h2>
 <ul>
 <li><a href='/senate#welcome'>Welcome!</a></li>
