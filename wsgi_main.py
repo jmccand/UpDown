048f8759bad6039dfa71ae44c26e0f73a4b0c7e9
Joel McCandless <mail@joelmccandless.com>
2022-03-10 20:04:09 -0500

    Make a basic service worker and register it Part of making a PWA (progressive web app) is to make a service worker and register it. This allows for prompting the user to install the app, push notifications on the device, and offline caching.

--- wsgi_main.py
+++ wsgi_main.py
@@ -410,6 +410,20 @@ Enter your school email (must end in @lexingtonma.org):
 </form>
 
 <script>
+if ('serviceWorker' in navigator) {{
+    window.addEventListener('load', function() {{
+        navigator.serviceWorker.register('service-worker.js').then(function(registration) {{
+        console.log('Registered!');
+        }}, function(err) {{
+            console.log('ServiceWorker registration failed: ', err);
+        }}).catch(function(err) {
+            console.log(err);
+        }});
+    }});
+}} else {{
+  console.log('service worker is not supported');
+}}
+
 exceptionEmails = {list(local.EXCEPTION_EMAILS)}
 setTimeout(checkEmail, 1000);
 function checkEmail() {{
