e74f35aab70d7ee79eef80c2b4f2446abf9887f9
Joel McCandless <mail@joelmccandless.com>
2022-04-17 14:56:27 -0400

    Update the pullbackup.py program to print more info

--- pullbackup.py
+++ pullbackup.py
@@ -4,9 +4,9 @@ import time
 import datetime
 
 while True:
-    time.sleep(local.DB_SLEEP_DELAY)
-    print(f'\nNow running rsync at time {datetime.datetime.now()}')
     run_command = f'rsync -rv --exclude "*.new" {local.PULL_BACKUP_FROM} {local.PULL_BACKUP_TO}'
+    print(f'\nNow running rsync at time {datetime.datetime.now()}\nCOMMAND: {run_command}')
     p = subprocess.run(run_command, shell=True, capture_output=True)
-    if p.returncode != 0:
-        print(f'{p.standarderr}\n{p.standardout}')
+    print(f'{p.stderr.decode("utf-8")}\n{p.stdout.decode("utf-8")}')
+    print('rsync done!')
+    time.sleep(local.DB_SLEEP_DELAY)
