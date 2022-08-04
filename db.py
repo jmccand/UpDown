68cb4662f06efa08616739fc76b2c4d250f4fab6
Joel McCandless <mail@joelmccandless.com>
2022-08-03 20:42:04 -0400

    Reorganize the locks to prepare for change in login Each device is going to have a separate cookie to improve ease of use.

--- db.py
+++ db.py
@@ -8,13 +8,16 @@ def open_db(filename):
     shelf = shelve.Shelf(db)
     return shelf
 
+# user_cookies = open_db(local.PATH_TO_COOKIES)
 user_ids = open_db(local.PATH_TO_IDS)
 opinions_database = open_db(local.PATH_TO_OPINIONS)
 cookie_database = open_db(local.PATH_TO_COOKIES)
 verification_links = open_db(local.PATH_TO_VERIFICATION)
 opinions_calendar = open_db(local.PATH_TO_CALENDAR)
 
-user_cookies_lock = threading.Lock()
+# user_cookies_lock = threading.Lock()
+user_ids_lock = threading.Lock()
 opinions_database_lock = threading.Lock()
+cookie_database_lock = threading.Lock()
 verification_links_lock = threading.Lock()
 opinions_calendar_lock = threading.Lock()
