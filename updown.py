1a137da559df76f174a0f9dd63fe3a11e181e310
Joel McCandless <mail@joelmccandless.com>
2022-08-30 17:22:13 -0400

    Fix count votes to use new verification method

--- updown.py
+++ updown.py
@@ -25,21 +25,27 @@ class Opinion:
         self.resolved = resolved
 
     def count_votes(self):
+        # prepare verified accounts set
+        verified_accounts = set()
+        for this_cookie, this_secure in db.cookie_database.items():
+            if this_secure[1] == 'verified':
+                verified_accounts.add(this_secure[0])
+
+        # count the votes
         up_votes = 0
         down_votes = 0
         abstains = 0
-        for user in db.user_ids.values():
-            if user.verified_result == True and str(self.ID) in user.votes:
-                    this_vote = user.votes[str(self.ID)][-1][0]
-                    #print(f'{user.email} has voted {this_vote}')
-                    if this_vote == 'up':
-                        up_votes += 1
-                    elif this_vote == 'down':
-                        down_votes += 1
-                    elif this_vote == 'abstain':
-                        abstains += 1
-                    else:
-                        raise ValueError(f'Found a vote other than up, down, or abstain: {this_vote}')
+        for this_user_id in verified_accounts:
+            if str(self.ID) in user.votes:
+                this_vote = user.votes[str(self.ID)][-1][0]
+                if this_vote == 'up':
+                    up_votes += 1
+                elif this_vote == 'down':
+                    down_votes += 1
+                elif this_vote == 'abstain':
+                    abstains += 1
+                else:
+                    raise ValueError(f'Found a vote other than up, down, or abstain: {this_vote}')
         return up_votes, down_votes, abstains
 
     def care_agree_percent(self):
