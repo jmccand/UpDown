2d4f8e50304669e0c29bb64705240c70cd7f6128
Joel McCandless <mail@joelmccandless.com>
2022-11-03 22:30:23 -0400

    Add db corruption fixers to db_corruption Added a function to remove cookies with no device info. Added a function to unverify all users.

--- db_corruption.py
+++ db_corruption.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import db
 import local
 import datetime
@@ -46,3 +48,66 @@ def last_healthy():
                 if not check_corruption(tuple(db.open_db(f'{local.BACKUP_DIRECTORY}/{filename}/{ending}', o_type='r') for ending in local.FILE_ENDINGS), raise_error=False):
                     return filename
     return local.LAUNCH_DATE
+
+def excise_corrupted(db_files):
+    corruption_cases = [[],[]]
+    ids_db, opinions_db, cookies_db, verification_db, calendar_db, device_db = db_files
+    for cookie, secure in cookies_db.items():
+        if not secure[0] in ids_db:
+            corruption_cases[0].append(cookie)
+        if not cookie in device_db:
+            corruption_cases[1].append(cookie)
+    for cookie in corruption_cases[0]:
+        print(f'    deleting cookie {cookie}; link to nonexistent account')
+        del cookies_db[cookie]
+    cookies_db.sync()
+    for cookie in corruption_cases[1]:
+        print(f'    deleting cookie {cookie}; no device data')
+        del cookies_db[cookie]
+    cookies_db.sync()
+    u_emails = set()
+    bad_verification = []
+    for u_id, u_account in ids_db.items():
+        u_emails.add(u_account.email)
+    for v_link, u_email in verification_db.items():
+        if u_email not in u_emails:
+            bad_verification.append((v_link, u_email))
+    for v_link, u_email in bad_verification:
+        print(f'    deleting verification link {v_link}; no account under email {u_email}')
+        del verification_db[v_link]
+    verification_db.sync()
+
+    print(f'    excised corrupted ({len(corruption_cases[0]) + len(corruption_cases[1]) + len(bad_verification)} cases)')
+
+def unverify_all(db_files):
+    ids_db, opinions_db, cookies_db, verification_db, calendar_db, device_db = db_files
+    u_cookies = list(cookies_db.keys())
+    unverify_count = 0
+    for u_cookie in u_cookies:
+        assert len(cookies_db[u_cookie]) == 2
+        if cookies_db[u_cookie][1] != 'unsure':
+            cookies_db[u_cookie] = (cookies_db[u_cookie][0], 'unsure')
+            unverify_count += 1
+    cookies_db.sync()
+    print(f'unverified {unverify_count} users')
+
+def main():
+    db.init(sys.argv[1])
+    if sys.argv[2] == 'check':
+        check_corruption((db.user_ids, db.opinions_database, db.cookie_database, db.verification_links, db.opinions_calendar, db.device_info))
+    elif sys.argv[2] == 'excise':
+        print('EXCISING DATABASE after 10 secs... make sure you have a backup. ctrl-c ctrl-c to cancel')
+        for count in range(10, 0, -1):
+            print(f'{count} secs...')
+            time.sleep(1)
+        excise_corrupted((db.user_ids, db.opinions_database, db.cookie_database, db.verification_links, db.opinions_calendar, db.device_info))
+    elif sys.argv[2] == 'unverify':
+        print('UNVERIFYING ALL USERS after 10 secs... make sure you have a basckup. ctrl-c ctrl-c to cancel')
+        for count in range(10, 0, -1):
+            print(f'{count} secs...')
+            time.sleep(1)
+        unverify_all((db.user_ids, db.opinions_database, db.cookie_database, db.verification_links, db.opinions_calendar, db.device_info))
+
+
+if __name__ == '__main__':
+    main()
