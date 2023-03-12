65cb2852c1f12e9c74e7d6464e3e652ede5bcd14
Joel McCandless <mail@joelmccandless.com>
2023-03-12 19:38:44 -0400

    Fix computer-generated opinions to avoid corruption

--- wsgi_main.py
+++ wsgi_main.py
@@ -3716,7 +3716,7 @@ def auto_schedule():
                         copy_opinion = db.opinions_database[new_random]
                         if new_random not in compiled_set and copy_opinion.approved == True:
                             def update_opinions_database():
-                                new_opinion = updown.Opinion(len(db.opinions_database), copy_opinion.text, [(-1, datetime.datetime.now())], approved=copy_opinion.approved, scheduled=True)
+                                new_opinion = updown.Opinion(len(db.opinions_database), copy_opinion.text, [(-1, datetime.datetime.now()), [(-1, 'yes', datetime.datetime.now())], [(next_due_date, datetime.datetime.now())]], approved=copy_opinion.approved, scheduled=True)
                                 db.opinions_database[new_opinion.ID] = new_opinion
                                 return new_opinion.ID
                             new_opinion_id = run_and_sync(db.opinions_database, update_opinions_database, db.opinions_database_lock)
@@ -3726,7 +3726,7 @@ def auto_schedule():
                         copy_opinion = db.opinions_database[opinion_ID]
                         if not smart_opinion_in(compiled_set, opinion_ID) and copy_opinion.approved == True:
                             def update_opinions_database():
-                                new_opinion = updown.Opinion(str(len(db.opinions_database)), copy_opinion.text, [(-1, datetime.datetime.now())], approved=copy_opinion.approved, scheduled=True)
+                                new_opinion = updown.Opinion(len(db.opinions_database), copy_opinion.text, [(-1, datetime.datetime.now()), [(-1, 'yes', datetime.datetime.now())], [(next_due_date, datetime.datetime.now())]], approved=copy_opinion.approved, scheduled=True)
                                 db.opinions_database[new_opinion.ID] = new_opinion
                                 return new_opinion.ID
                             new_opinion_id = run_and_sync(db.opinions_database_lock, update_opinions_database, db.opinions_database)
