b43c149f9dfa26c3795cf48ab4f0974507f1b626
Joel McCandless <mail@joelmccandless.com>
2023-05-24 20:59:36 -0400

    Add the --delete-source-files tag rsync So that we can save room on the server by not storing so many backups

--- pullbackup.py
+++ pullbackup.py
@@ -4,7 +4,7 @@ import time
 import datetime
 
 while True:
-    run_command = f'rsync -rv --exclude "*.new" {local.PULL_BACKUP_FROM} {local.PULL_BACKUP_TO}'
+    run_command = f'rsync -rv --exclude --remove-source-files "*.new" {local.PULL_BACKUP_FROM} {local.PULL_BACKUP_TO}'
     print(f'\nNow running rsync at time {datetime.datetime.now()}\nCOMMAND: {run_command}')
     p = subprocess.run(run_command, shell=True, capture_output=True)
     print(f'{p.stderr.decode("utf-8")}\n{p.stdout.decode("utf-8")}')
